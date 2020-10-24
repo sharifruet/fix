147bd5a86c828424cb9c03b56f40fe5c7125dcb9
Md. Hazrat Ali <mdhazrat445891@gmail.com>
2020-10-24 22:45:20 +0600

    Master data changed

--- node-back/app/models/init.js
+++ node-back/app/models/init.js
@@ -2,7 +2,6 @@ const db = require("../models");
 const data = require("./master_data.json");
 const AreaHierarchy = db.AreaHierarchy;
 const User = db.User;
-const ServiceHierarchy = db.serviceHierarchy;
 module.exports =  () => {
 	console.log("DB Initializations");
 	if(data.AreaHierarchy){
@@ -11,13 +10,6 @@ module.exports =  () => {
 			AreaHierarchy.create(ah).then((d)=>{console.log("Inserted");}).catch((e)=>{console.log("Error: "+e);});
 		});
 	}
-
-	// if(data.ServiceHierarchy){
-	// 	console.log("Inserting Service Hierarchy data");
-	// 	data.ServiceHierarchy.forEach((ah)=>{
-	// 		ServiceHierarchy.create(ah).then((d)=>{console.log("Inserted");}).catch((e)=>{console.log("Error: "+e);});
-	// 	});
-	// }
 
 	if(data.User){
 		console.log("Inserting User data");
